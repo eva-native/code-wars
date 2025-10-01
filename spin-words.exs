d79bd07f65704c1d63e72fd78a6eaf610cb84343
eva-native <evadunmire@gmail.com>
2025-10-01 11:05:19 +0200

    refactored spin_words

--- spin-words.exs
+++ spin-words.exs
@@ -2,15 +2,13 @@ defmodule Spin do
   def spin_words(message) do
     message
     |> String.split()
-    |> Enum.map(&spin/1)
-    |> Enum.join(" ")
+    |> Enum.map_join(" ", &spin/1)
   end
 
   defp spin(word) do
-    if String.length(word) >= 5 do
-      String.reverse(word)
-    else
-      word
+    cond do
+      String.length(word) >= 5 -> String.reverse(word)
+      true -> word
     end
   end
 end
